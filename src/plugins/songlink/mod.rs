4db687e5c2524b437a1d9585a1543577fbea34c9
Mattsi <mattsi.jansky@codurance.com>
2023-03-25 21:53:50 +0000

    Readability refactors for songlink plugin

--- src/plugins/songlink/mod.rs
+++ src/plugins/songlink/mod.rs
@@ -6,8 +6,14 @@ use async_trait::async_trait;
 use message_template::SongLinkMessageTemplate;
 use regex::Regex;
 use std::sync::Arc;
+use lazy_static::lazy_static;
 
 mod message_template;
+
+lazy_static! {
+    static ref SPOTIFY_MATCHER: Regex = Regex::new(r"https://open.spotify.com([-a-zA-Z0-9()@:%_\+.~#?&//=]*)*").unwrap();
+}
+const SONG_LINK_BASE_URL: &'static str = "https://song.link/s/";
 
 pub struct SongLinkPlugin {}
 
@@ -29,26 +35,18 @@ impl Plugin for SongLinkPlugin {
         match event.event {
             SlackEventCallbackBody::Message(msg) => {
                 let content = msg.content.unwrap().text.unwrap();
-
-                let spotify_matcher =
-                    Regex::new(r"https://open.spotify.com([-a-zA-Z0-9()@:%_\+.~#?&//=]*)*")
-                        .unwrap();
-                let captures = spotify_matcher.captures(&content);
+                let captures = SPOTIFY_MATCHER.captures(&content);
 
                 if captures.is_some() {
-                    let content = captures.unwrap().get(0).unwrap().as_str();
-                    let mut new_link = String::from("https://song.link/s/");
+                    let content = captures.unwrap().get(0).expect("regex capture should be present").as_str();
+                    let mut new_link = String::from(SONG_LINK_BASE_URL);
                     new_link.push_str(&content[31..]);
-                    println!(
-                        "================= I HEARD SPOTIFY! NEW LINK: {new_link} ================="
-                    );
+
                     let message = SongLinkMessageTemplate { url: new_link };
                     let ts = msg.origin.ts;
                     let channel = msg.origin.channel.unwrap();
 
                     Self::reply_to_thread(client, message, ts, channel).await?;
-                } else {
-                    println!("================= DID NOT REACT TO {content} =================")
                 }
             }
             _ => {}
