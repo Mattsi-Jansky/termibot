61a6143e53a7daee52746a70509e09ba8a7e6724
Mattsi <mattsi.jansky@codurance.com>
2023-03-25 19:57:09 +0000

    Remove placeholder code from on_command.rs

--- src/on_command.rs
+++ src/on_command.rs
@@ -11,37 +11,8 @@ pub async fn on_command_event(
     #[cfg(debug_assertions)]
     println!("COMMAND: {:#?}", event);
 
-    let token_value: SlackApiTokenValue = CONFIG.bot_token.clone().into();
-    let token: SlackApiToken = SlackApiToken::new(token_value);
-
-    // Sessions are lightweight and basically just a reference to client and token
-    let session = client.open_session(&token);
-
-    session
-        .api_test(&SlackApiTestRequest::new().with_foo("Test".into()))
-        .await?;
-
     Ok(SlackCommandEventResponse::new(
         SlackMessageContent::new()
-            .with_text("Working on it".into())
-            .with_blocks(slack_blocks![
-                some_into(SlackSectionBlock::new().with_text(md!(
-                    "Working section for {}",
-                    event.user_id.to_slack_format()
-                ))),
-                some_into(SlackActionsBlock::new(slack_blocks![
-                    some_into(SlackBlockButtonElement::new(
-                        "my-simple-action-button".into(),
-                        pt!("Action button")
-                    )),
-                    some_into(
-                        SlackBlockStaticSelectElement::new("my-simple-static-menu".into())
-                            .with_options(vec![SlackBlockChoiceItem::new(
-                                pt!("my-option1"),
-                                "my-option1-value".to_string()
-                            )])
-                    )
-                ]))
-            ]),
+            .with_text("ERROR: Not implemented yet".into())
     ))
 }
