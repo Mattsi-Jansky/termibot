532c59c918ddc4540b96a5dd56ed80ff8204f003
Mattsi <mattsi.jansky@codurance.com>
2023-03-26 18:46:14 +0100

    Cleanup for reply_thread test

--- src/actions/handlers/reply_thread.rs
+++ src/actions/handlers/reply_thread.rs
@@ -32,26 +32,27 @@ pub async fn reply_to_thread(
 mod tests {
     use super::*;
     use async_trait::async_trait;
+    use lazy_static::lazy_static;
     use mockall::mock;
 
-    #[tokio::test]
-    async fn wow() {
-        mock! {
-            #[async_trait]
-            Client {}
-            #[async_trait]
-            impl SlackBotClient for Client {
-                async fn post_message(&self, request: &SlackApiChatPostMessageRequest) -> ClientResult<SlackApiChatPostMessageResponse>;
-            }
+    mock! {
+        #[async_trait]
+        Client {}
+        #[async_trait]
+        impl SlackBotClient for Client {
+            async fn post_message(&self, request: &SlackApiChatPostMessageRequest) -> ClientResult<SlackApiChatPostMessageResponse>;
         }
-        mock! {
-            MessageTemplate {}     // Name of the mock struct, less the "Mock" prefix
-            impl SlackMessageTemplate for MessageTemplate {   // specification of the trait to mock
-                fn render_template(&self) -> SlackMessageContent;
-            }
-        }
+    }
 
-        let incoming_message_event = SlackMessageEvent {
+    mock! {
+        MessageTemplate {}     // Name of the mock struct, less the "Mock" prefix
+        impl SlackMessageTemplate for MessageTemplate {   // specification of the trait to mock
+            fn render_template(&self) -> SlackMessageContent;
+        }
+    }
+
+    lazy_static! {
+        static ref TEST_MESSAGE_EVENT: SlackMessageEvent = SlackMessageEvent {
             origin: SlackMessageOrigin {
                 ts: SlackTs::from("123"),
                 channel: Some(SlackChannelId::from("S0M3L0NG1D")),
@@ -71,6 +72,10 @@ mod tests {
             edited: None,
             deleted_ts: None,
         };
+    }
+
+    #[tokio::test]
+    async fn should_extract_origin_details_and_send_message_replying_to_thread() {
         let mut outgoing_message = MockMessageTemplate::new();
         outgoing_message
             .expect_render_template()
@@ -78,7 +83,9 @@ mod tests {
             .returning(|| SlackMessageContent::new());
         let outgoing_message = Box::new(outgoing_message);
         let mut client = MockClient::new();
-        client.expect_post_message().times(1).returning(|_| {
+        client.expect_post_message()
+            .times(1)
+            .returning(|_| {
             Ok(SlackApiChatPostMessageResponse {
                 channel: SlackChannelId::from("S0M3L0NG1D"),
                 ts: SlackTs::from("123"),
@@ -91,7 +98,8 @@ mod tests {
             })
         });
 
-        let result = reply_to_thread(&client, incoming_message_event, outgoing_message).await;
+        let result = reply_to_thread(&client, TEST_MESSAGE_EVENT.clone(), outgoing_message).await;
+        
         assert!(matches!(result, Ok(_)));
     }
 }
