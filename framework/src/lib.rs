664c946d0487693618bc866afa3161ce1d3dae38
Mattsi <mattsi.jansky@codurance.com>
2023-06-02 22:35:57 +0100

    Add variable for bot token in slack client

--- framework/src/lib.rs
+++ framework/src/lib.rs
@@ -1,24 +1,26 @@
 use reqwest::{Error, Response};
 
-//Example requesting all channels
-// client.get("https://slack.com/api/conversations.list")
-// .header("Authorization", "Bearer <bot token>")
-// .header("User-Agent", "slackbot-client")
-// .header("Accept", "application/json")
-// .send()
-// .await
+pub struct SlackClient {
+    bot_token: String
+}
 
-pub async fn send_thread_reply() -> Result<Response, Error> {
-    let client = reqwest::Client::new();
-    client.post("https://slack.com/api/chat.postMessage")
-        .header("Authorization", "Bearer ")
-        .header("User-Agent", "slackbot-client")
-        .header("Accept", "application/json")
-        .json(&serde_json::json!({
+impl SlackClient {
+    pub fn new(bot_token: &str) -> SlackClient {
+        SlackClient { bot_token: String::from(bot_token) }
+    }
+
+    pub async fn send_thread_reply(&self) -> Result<Response, Error> {
+        let client = reqwest::Client::new();
+        client.post("https://slack.com/api/chat.postMessage")
+            .header("Authorization", format!("Bearer {}", self.bot_token))
+            .header("User-Agent", "slackbot-client")
+            .header("Accept", "application/json")
+            .json(&serde_json::json!({
             "channel": "#bots",
             "thread_ts": "1685739918.465369",
             "text": "Hello world"
         }))
-        .send()
-        .await
+            .send()
+            .await
+    }
 }
--- framework/tests/acceptance.rs
+++ framework/tests/acceptance.rs
@@ -1,10 +1,11 @@
-use framework::send_thread_reply;
+use framework::SlackClient;
+
+const BOT_TOKEN: &str = "fill_here";
 
 #[tokio::test]
 async fn test() {
-    let result = send_thread_reply().await.unwrap();
+    let client = SlackClient::new(BOT_TOKEN);
+    let result = client.send_thread_reply().await.unwrap();
 
     println!("====== WAT: {:?}", result.text().await);
-    // println!("====== WOT: {:?}", result.headers());
-    // println!("====== STA: {:?}", result.status());
 }
