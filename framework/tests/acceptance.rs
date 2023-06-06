afbf92de9aee91362d28926dfae1806700fcd020
Mattsi <mattsi.jansky@codurance.com>
2023-06-06 13:47:31 +0100

    Extract test client builder

--- framework/tests/acceptance.rs
+++ framework/tests/acceptance.rs
@@ -1,15 +1,6 @@
-mod test_config;
+mod test_client_builder;
 
-use std::fs;
-use std::path::{Path};
-use glob::{glob};
-use regex::{Regex, Replacer};
-use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
-use rvcr::{VCRMiddleware, VCRMode};
-use framework::SlackClient;
-use crate::test_config::TEST_CONFIG;
-
-const FAKE_TOKEN: &str = "xoxn-not-a-real-token";
+use crate::test_client_builder::TestClientBuilder;
 
 #[tokio::test]
 async fn should_send_messages_to_channels_and_threads() {
@@ -20,52 +11,6 @@ async fn should_send_messages_to_channels_and_threads() {
     let result = result.unwrap();
 
     let new_message = &format!("replying to {}", result.message.text)[..];
-    let result2 = client.message_thread("#bots", &result.message, new_message);
-    assert!(result2.await.is_ok());
-}
-
-struct TestClientBuilder {
-    name: String
-}
-
-impl Drop for TestClientBuilder {
-    fn drop(&mut self) {
-        //On drop remove any secure credentials
-        let regex = Regex::new(r"(xoxb|xapp-1|xoxp|xoxa-2|xoxr)-([a-zA-Z0-9]+-?){3}").unwrap();
-        let files = glob(&format!("{}/tests/resources/*", env!("CARGO_MANIFEST_DIR"))[..]).unwrap();
-        for file in files {
-            let file = file.unwrap();
-            let path = file.as_path();
-            let contents = fs::read_to_string(path).expect("Cleaning cassette failed - DO NOT COMMIT!");
-            let cleaned_contents = regex.replace_all(&contents, FAKE_TOKEN).to_string();
-            fs::write(path, cleaned_contents).expect("Writing cleaned cassette failed - DO NOT COMMIT!");
-        }
-    }
-}
-
-impl TestClientBuilder {
-    fn new(name: &str) -> TestClientBuilder {
-        TestClientBuilder { name: name.to_string() }
-    }
-
-    fn new_client(&self) -> SlackClient {
-        let path = format!("{}/tests/resources/{}.vcr.json", env!("CARGO_MANIFEST_DIR"), self.name);
-        let path = Path::new(&path);
-        if path.exists() {
-            fs::remove_file(path).expect(&format!("Failed to delete old cassette {:?}", path)[..]);
-        }
-
-        let mut middleware = VCRMiddleware::try_from(path.to_path_buf())
-            .unwrap();
-        if TEST_CONFIG.is_record_mode {
-            middleware = middleware.with_mode(VCRMode::Record);
-        }
-
-        let vcr_client: ClientWithMiddleware = ClientBuilder::new(reqwest::Client::new())
-            .with(middleware)
-            .build();
-
-        let client = SlackClient::with_client(&TEST_CONFIG.bot_token[..], vcr_client);
-        client
-    }
+    let result2 = client.message_thread("#bots", &result.message, new_message).await;
+    assert!(result2.is_ok());
 }
---
+++ framework/tests/test_client_builder.rs
@@ -0,0 +1,69 @@
+use framework::SlackClient;
+use std::path::Path;
+use std::fs;
+use lazy_static::lazy_static;
+use rvcr::{VCRMiddleware, VCRMode};
+use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
+use regex::Regex;
+use serde::Deserialize;
+use config_file::FromConfigFile;
+
+#[derive(Deserialize)]
+pub struct TestConfig {
+    pub bot_token: String,
+    pub is_record_mode: bool
+}
+
+lazy_static! {
+    pub static ref TEST_CONFIG: TestConfig = TestConfig::from_config_file("config/config.toml").unwrap();
+}
+const FAKE_TOKEN: &str = "xoxn-not-a-real-token";
+
+pub struct TestClientBuilder {
+    name: String
+}
+
+impl Drop for TestClientBuilder {
+    fn drop(&mut self) {
+        //On drop remove any secure credentials
+        let regex = Regex::new(r"(xoxb|xapp-1|xoxp|xoxa-2|xoxr)-([a-zA-Z0-9]+-?){3}").unwrap();
+        let files = glob::glob(&format!("{}/tests/resources/*", env!("CARGO_MANIFEST_DIR"))[..]).unwrap();
+        for file in files {
+            let file = file.expect("Cleaning cassette failed - DO NOT COMMIT!");
+            let path = file.as_path();
+            let contents = fs::read_to_string(path).expect("Cleaning cassette failed - DO NOT COMMIT!");
+            let cleaned_contents = regex.replace_all(&contents, FAKE_TOKEN).to_string();
+            fs::write(path, cleaned_contents).expect("Writing cleaned cassette failed - DO NOT COMMIT!");
+        }
+    }
+}
+
+impl TestClientBuilder {
+    pub fn new(name: &str) -> TestClientBuilder {
+        TestClientBuilder { name: name.to_string() }
+    }
+
+    pub fn new_client(&self) -> SlackClient {
+        let path = format!("{}/tests/resources/{}.vcr.json", env!("CARGO_MANIFEST_DIR"), self.name);
+        let path = Path::new(&path);
+        if TEST_CONFIG.is_record_mode && path.exists() {
+            fs::remove_file(path).expect(&format!("Failed to delete old cassette {:?}", path)[..]);
+        }
+
+        let mut middleware = VCRMiddleware::try_from(path.to_path_buf())
+            .unwrap();
+        if TEST_CONFIG.is_record_mode {
+            middleware = middleware.with_mode(VCRMode::Record);
+        }
+
+        let vcr_client: ClientWithMiddleware = ClientBuilder::new(reqwest::Client::new())
+            .with(middleware)
+            .build();
+
+        if TEST_CONFIG.is_record_mode {
+            SlackClient::with_client(&TEST_CONFIG.bot_token[..], vcr_client)
+        } else {
+            SlackClient::with_client(&FAKE_TOKEN, vcr_client)
+        }
+    }
+}
