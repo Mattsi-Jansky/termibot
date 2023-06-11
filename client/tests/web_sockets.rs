a40d2a2a8a16419b69f5a6f07a88f2029ab225f3
Mattsi <mattsi.jansky@codurance.com>
2023-06-11 20:18:31 +0100

    Woops, that commit was premature

--- client/tests/web_sockets.rs
+++ client/tests/web_sockets.rs
@@ -3,10 +3,11 @@ mod test_client_builder;
 use crate::test_client_builder::TestClientBuilder;
 use client::models::{Event, Payload, SocketMessage};
 use client::SlackClient;
-use futures_util::SinkExt;
+use futures_util::{SinkExt, StreamExt};
 use slack_morphism::blocks::SlackBlock;
 use slack_morphism::{prelude, slack_block_item, slack_blocks};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::mpsc;
 use tokio::task::JoinHandle;
 use tokio_tungstenite::{accept_async, WebSocketStream};
 use tokio_tungstenite::tungstenite::Message;
@@ -14,7 +15,7 @@ use tokio_tungstenite::tungstenite::Message;
 const FAKE_SLACK_TEXT_MESSAGE: &str = "{\"envelope_id\":\"fake-enve-lope-i-d\",\"payload\":{\"token\":\"F4K3T0K3N\",\"team_id\":\"F4K3T34M1D\",\"context_team_id\":\"F4K3T34M1D\",\"context_enterprise_id\":null,\"api_app_id\":\"F4K34P1ID\",\"event\":{\"client_msg_id\":\"fake-client-msg-id\",\"type\":\"message\",\"text\":\"test\",\"user\":\"F4K3USER1D\",\"ts\":\"1686321337.206879\",\"blocks\":[{\"type\":\"rich_text\",\"block_id\":\"\\/5p\",\"elements\":[{\"type\":\"rich_text_section\",\"elements\":[{\"type\":\"text\",\"text\":\"test\"}]}]}],\"team\":\"F4K3T34M1D\",\"channel\":\"F4K3CH4NN3L1D\",\"event_ts\":\"1686321337.206879\",\"channel_type\":\"im\"},\"type\":\"event_callback\",\"event_id\":\"F4K33V3NT1D\",\"event_time\":1686321337,\"authed_users\":[\"F4K3USER1D\"],\"authorizations\":[{\"enterprise_id\":null,\"team_id\":\"F4K3T34M1D\",\"user_id\":\"F4K3USER1D\",\"is_bot\":true,\"is_enterprise_install\":false}],\"is_ext_shared_channel\":false,\"event_context\":\"4-fake-event-context\"},\"type\":\"events_api\",\"accepts_response_payload\":false,\"retry_attempt\":0,\"retry_reason\":\"\"}";
 const FAKE_HELLO_MESSAGE: &str = "{\"type\":\"hello\",\"num_connections\":1,\"debug_info\":{\"host\":\"applink-2\",\"build_number\":30,\"approximate_connection_time\":18060},\"connection_info\":{\"app_id\":\"fake-app-id\"}}";
 
-fn start_websocket_server() -> WebSocketStream<TcpStream> {
+async fn start_websocket_server() -> WebSocketStream<TcpStream> {
     let listener = TcpListener::bind("127.0.0.1:12345").await.unwrap();
     let (connection, _) = listener.accept().await.expect("No connections to accept");
     let stream = accept_async(connection).await;
@@ -25,7 +26,7 @@ fn start_websocket_server() -> WebSocketStream<TcpStream> {
 #[tokio::test]
 async fn should_initiate_socket_mode_connection() {
     let websocket_server = async move {
-        let mut stream = start_websocket_server();
+        let mut stream = start_websocket_server().await;
         stream
             .send(Message::Text(String::from(FAKE_HELLO_MESSAGE)))
             .await
@@ -34,11 +35,11 @@ async fn should_initiate_socket_mode_connection() {
             .send(Message::Text(String::from(FAKE_SLACK_TEXT_MESSAGE)))
             .await
             .unwrap();
+
     };
-    tokio::spawn(websocket_server)
+    let handle = tokio::spawn(websocket_server);
     let builder = TestClientBuilder::new("should_initiate_socket_mode_connection");
     let client = builder.new_client();
-    let handle = hmm();
     let mut listener = client.connect_to_socket_mode().await.unwrap();
 
     let result = listener.next().await;
