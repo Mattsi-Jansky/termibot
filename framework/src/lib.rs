4b52407672f4cc22da8ad671092492ce6e3cd753
Mattsi <mattsi.jansky@codurance.com>
2023-06-13 20:58:36 +0100

    framework: Implement calling plugins on events

--- framework/src/lib.rs
+++ framework/src/lib.rs
@@ -1,18 +1,22 @@
+extern crate core;
+
+use async_trait::async_trait;
+use mockall::automock;
 use client::error::SlackClientError;
 use client::message::Message;
-use client::models::SocketMessage;
-use client::SlackClient;
+use client::models::{Event, SocketMessage};
 use client::socket_listener::SocketModeListener;
+use client::SlackClient;
 
 pub struct SlackBot {
-    client: Box<dyn SlackClient>,
+    client: Box<dyn SlackClient + Sync>,
     listener: Box<dyn SocketModeListener>,
+    plugins: Vec<Box<dyn Plugin>>
 }
 
 impl SlackBot {
-    pub fn from(client: Box<dyn SlackClient>,
-                listener: Box<dyn SocketModeListener>) -> SlackBot {
-        SlackBot {client, listener}
+    pub fn from(client: Box<dyn SlackClient + Sync>, listener: Box<dyn SocketModeListener>) -> SlackBot {
+        SlackBot { client, listener, plugins: vec![] }
     }
 
     pub async fn run(self) -> Result<(), SlackClientError> {
@@ -24,12 +28,18 @@ impl SlackBot {
             match message {
                 SocketMessage::Event {
                     envelope_id,
-                    payload: _,
+                    payload,
                 } => {
-                    todo!()
+                    for plugin in &self.plugins {
+                        plugin.on_event(&payload.event, &self.client);
+                    }
                 }
-                SocketMessage::Interactive { .. } => { todo!()/*Not implemented*/ }
-                SocketMessage::SlashCommand { .. } => { todo!()/*Not implemented*/ }
+                SocketMessage::Interactive { .. } => {
+                    todo!() /*Not implemented*/
+                }
+                SocketMessage::SlashCommand { .. } => {
+                    todo!() /*Not implemented*/
+                }
 
                 SocketMessage::Hello { .. } => { /* Nothing to do */ }
                 SocketMessage::Disconnect { .. } => {
@@ -40,38 +50,67 @@ impl SlackBot {
 
         Ok(())
     }
+
+    pub(crate) fn with(mut self, plugin: Box<dyn Plugin>) -> Self {
+        self.plugins.push(plugin);
+        self
+    }
+}
+
+pub enum Action {
+    DoNothing
+}
+
+#[async_trait]
+#[automock]
+pub trait Plugin {
+    async fn on_event(&self, event: &Event, client: &Box<dyn SlackClient + Sync>) -> Action; //todo: return command pattern
 }
 
 #[cfg(test)]
 mod tests {
+    use std::future;
+    use super::*;
     use async_trait::async_trait;
     use client::error::SlackClientError;
-    use client::models::SocketMessage;
+    use client::models::{Payload, SocketMessage};
     use client::response::ApiResponse;
-    use super::*;
 
-    struct TestSlackClient {}
+    struct TestSlackClient { message: String }
     impl Default for TestSlackClient {
         fn default() -> Self {
-            TestSlackClient {}
+            TestSlackClient { message: String::new() }
         }
     }
     #[async_trait]
     impl SlackClient for TestSlackClient {
-        async fn message_channel(&self, channel: &str, message: &str) -> Result<ApiResponse, SlackClientError> {
+        async fn message_channel(
+            &self,
+            channel: &str,
+            message: &str,
+        ) -> Result<ApiResponse, SlackClientError> {
             todo!()
         }
 
-        async fn message_thread(&self, channel: &str, parent: &Message, message: &str) -> Result<ApiResponse, SlackClientError> {
+        async fn message_thread(
+            &self,
+            channel: &str,
+            parent: &Message,
+            message: &str,
+        ) -> Result<ApiResponse, SlackClientError> {
             todo!()
         }
 
-        async fn connect_to_socket_mode(&self) -> Result<Box<dyn SocketModeListener>, SlackClientError> {
+        async fn connect_to_socket_mode(
+            &self,
+        ) -> Result<Box<dyn SocketModeListener>, SlackClientError> {
             Ok(Box::new(TestSocketModeListener::default()))
         }
     }
 
-    struct TestSocketModeListener { call_count: usize }
+    struct TestSocketModeListener {
+        call_count: usize,
+    }
     impl Default for TestSocketModeListener {
         fn default() -> Self {
             TestSocketModeListener { call_count: 0 }
@@ -84,8 +123,22 @@ mod tests {
 
             if self.call_count == 1 {
                 Ok(SocketMessage::Hello {})
-            }
-            else {
+            } else if self.call_count == 2 {
+                Ok(SocketMessage::Event {
+                    envelope_id: "fake-envelope-id".to_string(),
+                    payload: Payload {
+                        event: Event {
+                            id: "fake-id".to_string(),
+                            event_type: "fake-id".to_string(),
+                            text: Some("this is your secret message".to_string()),
+                            user: None,
+                            blocks: vec![],
+                            channel: None,
+                            channel_type: None,
+                        },
+                    },
+                })
+            } else {
                 Ok(SocketMessage::Disconnect {})
             }
         }
@@ -95,8 +148,23 @@ mod tests {
     async fn disconnect_after_disconnect_message_received() {
         let bot = SlackBot::from(
             Box::new(TestSlackClient::default()),
-            Box::new(TestSocketModeListener::default())
+            Box::new(TestSocketModeListener::default()),
         );
+
+        bot.run().await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn forward_event_message_to_plugin() {
+        let mut mock_plugin = Box::new(MockPlugin::new());
+        mock_plugin.expect_on_event()
+            .times(1)
+            .returning(|_, _| Box::pin(future::ready(Action::DoNothing)));
+        let bot = SlackBot::from(
+            Box::new(TestSlackClient::default()),
+            Box::new(TestSocketModeListener::default()),
+        )
+            .with(mock_plugin);
 
         bot.run().await.unwrap();
     }
