340f0a51cf3299bc7e1e2806ef45891736135631
Mattsi <mattsi.jansky@codurance.com>
2023-06-22 20:03:29 +0100

    Parse emoji add events

--- client/src/models/socket_message.rs
+++ client/src/models/socket_message.rs
@@ -27,18 +27,39 @@ pub struct Payload {
     pub event: Event,
 }
 
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum Event {
+    Message(MessageEvent),
+    EmojiChanged(EmojiChangedEvent)
+}
+
 #[skip_serializing_none]
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
-pub struct Event {
+pub struct MessageEvent {
     #[serde(rename = "event_ts")]
     pub id: MessageId,
-    #[serde(rename = "type")]
-    pub event_type: String,
     pub text: Option<String>,
     pub user: Option<String>,
     pub blocks: Vec<Block>,
     pub channel: Option<String>,
     pub channel_type: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+#[serde(tag = "subtype", rename_all = "snake_case")]
+pub enum EmojiChangedEvent {
+    Add(AddEmojiEvent),
+    Remove,
+    Rename
+}
+
+#[skip_serializing_none]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+pub struct AddEmojiEvent {
+    #[serde(rename = "event_ts")]
+    pub id: MessageId,
+    name: String
 }
 
 #[cfg(test)]
@@ -56,23 +77,30 @@ mod tests {
     fn should_parse_message_event() {
         let result: Event = serde_json::from_str(FAKE_NEW_MESSAGE_EVENT).unwrap();
 
-        assert_eq!(result.id, "1687458843.576569".into());
-        assert_eq!(result.text.unwrap(), "wat".to_string());
-        assert_eq!(result.blocks, vec![Block::RichText(RichTextBlock::new()
-            .elements(vec![BlockElement::RichTextSection(
-                RichTextSectionElement::new()
-                    .elements(vec![BlockElement::Text(
-                        TextBody::new().text("wat".to_string()).build()
-                    )])
-                    .build()
-            )])
-            .build())]);
+        if let Event::Message(message) = result {
+            assert_eq!(message.id, "1687458843.576569".into());
+            assert_eq!(message.text.unwrap(), "wat".to_string());
+            assert_eq!(message.blocks, vec![Block::RichText(RichTextBlock::new()
+                .elements(vec![BlockElement::RichTextSection(
+                    RichTextSectionElement::new()
+                        .elements(vec![BlockElement::Text(
+                            TextBody::new().text("wat".to_string()).build()
+                        )])
+                        .build()
+                )])
+                .build())]);
+        } else { panic!("Wrong type of event") }
     }
 
     #[test]
     fn should_parse_new_emoji_event() {
         let result: Event = serde_json::from_str(FAKE_NEW_EMOJI_EVENT).unwrap();
 
-        assert_eq!(result.id, "1687458875.040100".into());
+        if let Event::EmojiChanged(result) = result {
+            if let EmojiChangedEvent::Add(result) = result {
+                assert_eq!(result.id, "1687458875.040100".into());
+                assert_eq!(result.name, "blobcat_knife".to_string());
+            } else { panic!("Wrong type of event") }
+        } else { panic!("Wrong type of event") }
     }
 }
