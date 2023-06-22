382b2ca5f2ee74c306717c000486854762d7cb06
Mattsi <mattsi.jansky@codurance.com>
2023-06-22 20:15:53 +0100

    Parse renamed emoji event

--- client/src/models/socket_message.rs
+++ client/src/models/socket_message.rs
@@ -51,7 +51,7 @@ pub struct MessageEvent {
 pub enum EmojiChangedEvent {
     Add(AddEmojiEvent),
     Remove(RemoveEmojiEvent),
-    Rename
+    Rename(RenameEmojiEvent)
 }
 
 #[skip_serializing_none]
@@ -70,6 +70,15 @@ pub struct RemoveEmojiEvent {
     pub names: Vec<String>
 }
 
+#[skip_serializing_none]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+pub struct RenameEmojiEvent {
+    #[serde(rename = "event_ts")]
+    pub id: MessageId,
+    pub old_name: String,
+    pub new_name: String
+}
+
 #[cfg(test)]
 mod tests {
     use crate::models::blocks::elements::BlockElement;
@@ -81,6 +90,7 @@ mod tests {
     const FAKE_NEW_MESSAGE_EVENT: &str = "{ \"client_msg_id\": \"aa022dae-607c-4e24-b0e1-f96c08855f4f\", \"type\": \"message\", \"text\": \"wat\", \"user\": \"U118BF6LQ\", \"ts\": \"1687458843.576569\", \"blocks\": [ { \"type\": \"rich_text\", \"block_id\": \"ZrfB\", \"elements\": [ { \"type\": \"rich_text_section\", \"elements\": [ { \"type\": \"text\", \"text\": \"wat\" } ] } ] } ], \"team\": \"T0G5PM4NR\", \"channel\": \"DEAS25LNP\", \"event_ts\": \"1687458843.576569\", \"channel_type\": \"im\"}";
     const FAKE_NEW_EMOJI_EVENT: &str = "{ \"type\": \"emoji_changed\", \"subtype\": \"add\", \"name\": \"blobcat_knife\", \"value\": \"https://emoji.slack-edge.com/T0G5PM4NR/blobcat_knife/8ce3359f5936936a.png\", \"event_ts\": \"1687458875.040100\"}";
     const FAKE_REMOVED_EMOJI_EVENT: &str = "{	\"type\": \"emoji_changed\",	\"subtype\": \"remove\",	\"names\": [\"picard_facepalm\"],	\"event_ts\" : \"1361482916.000004\"}";
+    const FAKE_RENAMED_EMOJI_EVENT: &str = "{	\"type\": \"emoji_changed\",	\"subtype\": \"rename\",	\"old_name\": \"grin\",	\"new_name\": \"cheese-grin\",	\"value\": \"https://my.slack.com/emoji/picard_facepalm/db8e287430eaa459.gif\",	\"event_ts\" : \"1361482916.000004\"}";
 
     #[test]
     fn should_parse_message_event() {
@@ -123,5 +133,18 @@ mod tests {
                 assert_eq!(result.names, vec!["picard_facepalm".to_string()]);
             } else { panic!("Wrong type of event") }
         } else { panic!("Wrong type of event") }
+    }
+
+    #[test]
+    fn should_parse_renamed_emoji_event() {
+        let result: Event = serde_json::from_str(FAKE_RENAMED_EMOJI_EVENT).unwrap();
+
+        if let Event::EmojiChanged(result) = result {
+            if let EmojiChangedEvent::Rename(result) = result {
+                assert_eq!(result.id, "1361482916.000004".into());
+                assert_eq!(result.old_name, "grin".to_string());
+                assert_eq!(result.new_name, "cheese-grin".to_string())
+            }
+        }
     }
 }
