079c0f24a8708a90fad3136d7a0cfd0338686ba7
Mattsi <mattsi.jansky@codurance.com>
2023-09-10 12:34:00 +0100

    Basic 'do nothing' test for karma plugin

--- karma/src/lib.rs
+++ karma/src/lib.rs
@@ -9,16 +9,44 @@ pub struct KarmaPlugin {
     downvote_emoji: String,
 }
 
+impl Default for KarmaPlugin {
+    fn default() -> Self {
+        KarmaPlugin {
+            upvote_emoji: String::from("upboat"),
+            downvote_emoji: String::from("downboat"),
+        }
+    }
+}
+
 #[async_trait]
 impl Plugin for KarmaPlugin {
     async fn on_event(&self, event: &Event, dependencies: &Dependencies) -> Vec<Action> {
-        todo!()
+        vec![]
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use client::models::message_id::MessageId;
+    use client::models::socket_message::MessageEvent;
+    use framework::dependencies::DependenciesBuilder;
+    use regex::internal::Input;
 
+    #[tokio::test]
+    async fn given_no_karma_change_do_nothing() {
+        let dependencies = DependenciesBuilder::default().build();
+        let event = Event::Message(MessageEvent {
+            id: MessageId("myMessageId".to_string()),
+            text: Some("test message".to_string()),
+            user: None,
+            blocks: None,
+            channel: None,
+            channel_type: None,
+        });
 
+        let result = KarmaPlugin::default().on_event(&event, &dependencies).await;
+
+        assert_eq!(0, result.len())
+    }
 }
