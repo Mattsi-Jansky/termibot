63da65dcc03f395d3a6ff26561c6287b7357ed12
Mattsi <mattsi.jansky@codurance.com>
2023-09-16 16:12:06 +0100

    Customise upvote emoji

--- karma/src/lib.rs
+++ karma/src/lib.rs
@@ -16,6 +16,15 @@ pub struct KarmaPlugin {
     downvote_emoji: String,
 }
 
+impl KarmaPlugin {
+    fn new(upvote_emoji: &str, downvote_emoji: &str) -> KarmaPlugin {
+        KarmaPlugin {
+            upvote_emoji: upvote_emoji.to_string(),
+            downvote_emoji: downvote_emoji.to_string(),
+        }
+    }
+}
+
 impl Default for KarmaPlugin {
     fn default() -> Self {
         KarmaPlugin {
@@ -36,9 +45,10 @@ impl Plugin for KarmaPlugin {
             for capture in KARMA_MATCHER.captures_iter(&text[..]) {
                 let capture = capture.get(0).unwrap().as_str();
                 let thing = &capture[..capture.len() - 2];
+                let emoji = &self.upvote_emoji;
                 results.push(Action::MessageChannel {
                     channel: "".to_string(),
-                    message: MessageBody::from_text(&format!(":upboat: {thing}: 1")[..]),
+                    message: MessageBody::from_text(&format!(":{emoji}: {thing}: 1")[..]),
                 });
             }
         }
@@ -73,7 +83,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn given_karma_change_return_karma_changed_message() {
+    async fn given_karma_change_should_return_karma_changed_message() {
         let dependencies = DependenciesBuilder::default().build();
         let event = Event::Message(MessageEvent {
             id: MessageId("myMessageId".to_string()),
@@ -92,6 +102,34 @@ mod tests {
             &Action::MessageChannel {
                 channel: "".to_string(),
                 message: MessageBody::from_text(":upboat: sunnydays: 1"),
+            },
+            result.get(0).unwrap()
+        );
+    }
+
+    #[tokio::test]
+    async fn given_override_of_upvote_emoji_should_return_karma_changed_message_with_custom_emoji()
+    {
+        let dependencies = DependenciesBuilder::default().build();
+        let event = Event::Message(MessageEvent {
+            id: MessageId("myMessageId".to_string()),
+            text: Some("sunnydays++".to_string()),
+            user: None,
+            blocks: None,
+            channel: None,
+            channel_type: None,
+        });
+
+        let result = KarmaPlugin::new("up_custom", "down_custom")
+            .on_event(&event, &dependencies)
+            .await;
+
+        dbg!(&result);
+        assert_eq!(1, result.len());
+        assert_eq!(
+            &Action::MessageChannel {
+                channel: "".to_string(),
+                message: MessageBody::from_text(":up_custom: sunnydays: 1"),
             },
             result.get(0).unwrap()
         );
