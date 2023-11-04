58865af7177e83ad1985b4d81a2e9653c8156711
Mattsi <mattsi.jansky@codurance.com>
2023-11-04 16:35:18 +0000

    Parse emoji in rich text blocks

--- client/src/models/blocks/elements/mod.rs
+++ client/src/models/blocks/elements/mod.rs
@@ -2,8 +2,10 @@ use crate::models::blocks::elements::text::LinkElement;
 use crate::models::blocks::objects::text::TextBody;
 use serde::{Deserialize, Serialize};
 use text::RichTextSectionElement;
+use crate::models::blocks::elements::emoji::EmojiElement;
 
 pub mod text;
+pub mod emoji;
 
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
 #[serde(tag = "type", rename_all = "snake_case")]
@@ -11,4 +13,5 @@ pub enum BlockElement {
     RichTextSection(RichTextSectionElement),
     Text(TextBody),
     Link(LinkElement),
+    Emoji(EmojiElement)
 }
--- client/src/models/blocks/mod.rs
+++ client/src/models/blocks/mod.rs
@@ -21,8 +21,13 @@ mod tests {
     use super::*;
     
     use crate::models::blocks::Block;
+    use crate::models::blocks::elements::BlockElement;
+    use crate::models::blocks::elements::emoji::EmojiElement;
+    use crate::models::blocks::elements::text::RichTextSectionElement;
+    use crate::models::blocks::objects::text::TextBody;
 
     const EMOJI_CHANGELOG_MESSAGE: &str = "[ { \"type\": \"header\", \"text\": { \"type\": \"plain_text\", \"text\": \"Emoji changelog\" } }, { \"type\": \"section\", \"text\": { \"type\": \"mrkdwn\", \"text\": \":heavy_plus_sign: :smile: `:smile:`\\n        By <@userid>\" } } ] ";
+    const RICH_TEXT_BLOCK_WITH_EMOJI: &str = "[{\"type\":\"rich_text\",\"block_id\":\"+QITd\",\"elements\":[{\"type\":\"rich_text_section\",\"elements\":[{\"type\":\"emoji\",\"name\":\"mild-panic\"},{\"type\":\"text\",\"text\":\"++\"}]}]}]";
 
     #[test]
     fn should_parse_emoji_changelog_message() {
@@ -35,6 +40,25 @@ mod tests {
                 SectionBlock::new_markdown(
                     ":heavy_plus_sign: :smile: `:smile:`\n        By <@userid>"
                 )
+            ]
+        )
+    }
+
+    #[test]
+    fn  should_parse_emoji_in_rich_text() {
+        let result: Vec<Block> = serde_json::from_str(RICH_TEXT_BLOCK_WITH_EMOJI).unwrap();
+
+        assert_eq!(
+            result,
+            vec![
+                Block::RichText(RichTextBlock { elements: vec![
+                    BlockElement::RichTextSection(RichTextSectionElement {
+                        elements: vec![
+                            BlockElement::Emoji(EmojiElement { name: "mild-panic".to_string() }),
+                            BlockElement::Text(TextBody { text: "++".to_string() })
+                        ],
+                    })
+                ]})
             ]
         )
     }
