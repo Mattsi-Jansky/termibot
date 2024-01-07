1fb4dc73460b8bea5728a7013619e7376f0dcf21
Mattsi <mattsi.jansky@codurance.com>
2024-01-07 17:26:22 +0000

    Cargo fmt

--- client/src/models/mod.rs
+++ client/src/models/mod.rs
@@ -3,5 +3,5 @@ pub mod http_response;
 pub mod message_body;
 pub mod message_id;
 pub mod socket_message;
-pub(crate) mod websocket_url_message;
 pub(crate) mod user_message;
+pub(crate) mod websocket_url_message;
--- client/src/models/user_message.rs
+++ client/src/models/user_message.rs
@@ -4,5 +4,5 @@ use serde::{Deserialize, Serialize};
 /// Corresponds to response of auth.test method: https://api.slack.com/methods/auth.test
 #[derive(Debug, PartialEq, Deserialize, Serialize, Clone)]
 pub struct UserDetailsMessage {
-    pub user_id: String
+    pub user_id: String,
 }
